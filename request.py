f03287cfeb734265954a64580d1d0dae17e307f2
paradmin <nick@nparadis.ca>
2018-10-14 09:03:48 -0400

    added khconf parameters

--- request.py
+++ request.py
@@ -1,2 +1,14 @@
-msg = "Hello World!"
-print(msg)
+import requests
+
+r = requests.get('https://report.khconf.com/api.php/info', auth=('1ParadisN', 'Zatnktel21**'))
+
+Status_Code = r.status_code
+output = r.json()
+
+print(Status_Code)
+print(output)
+
+
+
+
+# r.json()
